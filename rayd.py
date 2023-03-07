46d597ab5f55de104919631dd81e2f744a7c7711
chatonmars <95390641+chatonmars@users.noreply.github.com>
2023-03-07 20:38:45 +0100

    🔊 Log errors in log file

--- rayd.py
+++ rayd.py
@@ -1,6 +1,15 @@
+import logging
 import requests
 from bs4 import BeautifulSoup
 from yt_dlp import YoutubeDL
+
+logging.basicConfig(
+    filename='rayd.log',
+    encoding='utf-8',
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y/%m/%d %I:%M:%S %p',
+    level=logging.INFO)
+logging.info('Script starting')
 
 ydl_opts_default = {
     'download_archive': './yt_dlp_archive',
@@ -8,8 +17,9 @@ ydl_opts_default = {
     'paths': {
         'home': '~/Youtube'
     },
-    'windowsfilename': True
-}
+    'windowsfilename': True,
+    'ignoreerrors': True,
+    'logger': logging.getLogger()}
 
 
 ydl_opts_playlist = {
@@ -17,12 +27,16 @@ ydl_opts_playlist = {
     'outtmpl': '%(playlist_uploader)s/%(playlist)s/' + ydl_opts_default['outtmpl'],
     'paths': {
         'home': ydl_opts_default['paths']['home']},
-    'windowsfilename': ydl_opts_default['windowsfilename']}
+    'windowsfilename': ydl_opts_default['windowsfilename'],
+    'ignoreerrors': ydl_opts_default['ignoreerrors'],
+    'logger': ydl_opts_default['logger']}
 
 playlist_file = open('playlists.txt', 'r', encoding='utf8')
 for link in playlist_file:
     with YoutubeDL(ydl_opts_playlist) as yt_dl:
         yt_dl.download([link])
+
+playlist_file.close()
 
 
 ydl_opts_channel = {
@@ -30,13 +44,26 @@ ydl_opts_channel = {
     'outtmpl': '%(uploader)s/' + ydl_opts_default['outtmpl'],
     'paths': {
         'home': ydl_opts_default['paths']['home']},
-    'windowsfilename': ydl_opts_default['windowsfilename']}
+    'windowsfilename': ydl_opts_default['windowsfilename'],
+    'ignoreerrors': ydl_opts_default['ignoreerrors'],
+    'logger': ydl_opts_default['logger']}
 
 feed_file = open('rss.txt', 'r', encoding='utf8')
 for line in feed_file:
-    request_response = requests.get(line, timeout=60)
-    request_response.raise_for_status()
+    rss_link = line.strip()
+    request_response = requests.get(rss_link, timeout=60)
+    if request_response.status_code != 200:
+        logging.error(
+            '%d: %s // For link: %s',
+            request_response.status_code, request_response.reason, rss_link)
+        continue
     xml_tree = BeautifulSoup(request_response.text, 'xml')
+
+    if xml_tree.find('entry') is None:
+        logging.warning(
+            'Link: %s seems to not correspond to a rss feed of a Youtube channel',
+            rss_link)
+        continue
 
     for video in xml_tree.find_all('entry'):
         video_link = video.link.get('href')
@@ -44,3 +71,5 @@ for line in feed_file:
             yt_dl.download([video_link])
 
 feed_file.close()
+
+logging.info('Script ended')

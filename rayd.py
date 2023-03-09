d4a4ed6803663ce4469d4c992eccff9ddd462abe
chatonmars <95390641+chatonmars@users.noreply.github.com>
2023-03-09 21:00:27 +0100

    📝 Add documentation in Python's script

--- rayd.py
+++ rayd.py
@@ -1,3 +1,21 @@
+"""RAYD - RSS Automatic Youtube Downloader
+
+This script downloads from Youtube the latest videos of a channel via
+its RSS feed. It can also download videos from a playlist via its URL.
+
+The script looks for RSS feeds of Youtube channels in './rss.txt' and
+for URL of Youtube playlists in './playlists.txt'. There should be one
+link per line.
+
+This script was written and tested with Python 3.11 in a Windows
+environment but should work with older Python 3 versions and in a Linux
+environment.
+
+This script requires that 'requests', 'beautifulsoup4', and 'yt-dlp' be
+installed within the Python environment you are running this script in,
+they can be installed with pip.
+"""
+
 import logging
 import argparse
 import os
@@ -23,6 +41,7 @@ if not os.path.exists(args.download_folder):
     exit()
 
 
+# Config for the Root Logger Object
 logging.basicConfig(
     filename='rayd.log',
     encoding='utf-8',
@@ -31,6 +50,7 @@ logging.basicConfig(
     level=logging.INFO)
 logging.info('Script starting')
 
+# Default yt-dlp config
 ydl_opts_default = {
     'download_archive': './yt_dlp_archive',
     'outtmpl': '%(title)s.%(ext)s',
@@ -42,6 +62,8 @@ ydl_opts_default = {
     'logger': logging.getLogger()}
 
 
+# Deep copy default config and change the naming template of the
+# downloaded files to place them in a folder named after the playlist
 ydl_opts_playlist = {
     'download_archive': ydl_opts_default['download_archive'],
     'outtmpl': '%(playlist_uploader)s/%(playlist)s/' + ydl_opts_default['outtmpl'],
@@ -59,6 +81,8 @@ for link in playlist_file:
 playlist_file.close()
 
 
+# Deep copy default config and change the naming template of the
+# downloaded files to place them in a folder named after the uploader
 ydl_opts_channel = {
     'download_archive': ydl_opts_default['download_archive'],
     'outtmpl': '%(uploader)s/' + ydl_opts_default['outtmpl'],
@@ -72,6 +96,7 @@ feed_file = open('rss.txt', 'r', encoding='utf8')
 for line in feed_file:
     rss_link = line.strip()
     request_response = requests.get(rss_link, timeout=60)
+    # Check if request failed and continue to next link if so
     if request_response.status_code != 200:
         logging.error(
             '%d: %s // For link: %s',
@@ -79,6 +104,7 @@ for line in feed_file:
         continue
     xml_tree = BeautifulSoup(request_response.text, 'xml')
 
+    # In a Youtube RSS feed 'entry' represent a video
     if xml_tree.find('entry') is None:
         logging.warning(
             'Link: %s seems to not correspond to a rss feed of a Youtube channel',
